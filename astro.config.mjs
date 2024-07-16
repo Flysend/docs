4a384927e61353aa751672c37f7db8f60f53b3a5
Fishelle Rin <mr.rybka06@gmail.com>
2024-07-16 18:07:44 +0200

    here we go x2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,41 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Flysend Docs',
+    customCss: [
+      './src/tailwind.css',
+    ],
+    logo: {
+      light: '/public/fsdocs-black.svg',
+      dark: '/public/fsdocs-white.svg',
+      replacesTitle: true
+    },
+    sidebar: [{
+      label: 'Quick Start',
+      items: [
+      {
+        label: 'Introduction',
+        slug: 'start/intro'
+      }, {
+        label: 'References',
+        slug: 'start/references'
+      }]
+    }, {
+      label: 'Resources',
+      items: [{
+        label: 'User',
+        slug: 'resources/user'
+      }, {
+        label: 'Spaces',
+        slug: 'resources/spaces'
+      }]
+    }]
+  }), tailwind({
+    applyBaseStyles: false,
+  })]
 });
---
+++ src/content/docs/start/references.md
@@ -0,0 +1,7 @@
+---
+title: References
+---
+
+The API base URL for Flysend is `https://flysend.tpm.pw:3000`
+
+All requests have the header Content-Type `application/json`
